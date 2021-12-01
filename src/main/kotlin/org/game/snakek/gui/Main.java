4fbc2db087c0cd6f92db8d97a02b9d2841ea83d4
c2p-cmd <sharandt19@live.com>
2021-12-01 22:50:29 +0530

    Added game icon

--- src/main/kotlin/org/game/snakek/gui/Main.java
+++ src/main/kotlin/org/game/snakek/gui/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.util.Objects;
@@ -22,7 +23,11 @@ public class Main extends Application implements Runnable{
         stage.setWidth(500);
         stage.setHeight(625);
         stage.setScene(scene);
-
+        stage.getIcons().add(
+                new Image(
+                        Objects.requireNonNull(getClass().getResourceAsStream("snake-game.png"))
+                )
+        );
         stage.show();
     }
 
